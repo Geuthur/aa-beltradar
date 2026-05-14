5fe6ae8aecb7a5c4b2c55469638fa0a550d7d6ab
Geuthur <geuthur@users.noreply.github.com>
2026-05-14 12:05:35 +0200

    [CHANGE] Update PACKAGE name to 'beltradar' in local settings

--- testauth/settings/local.py
+++ testauth/settings/local.py
@@ -8,7 +8,7 @@ Test settings
 
 from .base import *
 
-PACKAGE = "example"
+PACKAGE = "beltradar"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/

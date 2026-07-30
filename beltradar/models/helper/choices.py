6f050e91bc3605fe93f2ac7eac1357393ede96ad
Geuthur <geuthur@users.noreply.github.com>
2026-07-30 17:00:19 +0200

    [ADD] Create helper choices for belt types and sizes

---
+++ beltradar/models/helper/choices.py
@@ -0,0 +1,25 @@
+"""Helper choices for the Beltradar models."""
+
+# Django
+from django.db import models
+
+
+class BeltTypeChoice(models.TextChoices):
+    """Choices for the belt type."""
+
+    ASTEROID_BELT = "asteroid_belt", "Asteroid Belt"
+    ICE_BELT = "ice_belt", "Ice Belt"
+    MERCOXIT_BELT = "mercobelt", "Mercocit Belt"
+    GAS_BELT = "gas_belt", "Gas Belt"
+    ARREY_BELT = "arrey_belt", "Arrey Belt"
+
+
+class BeltSizeChoice(models.TextChoices):
+    """Choices for the belt size."""
+
+    SMALL = "small", "Small"
+    MEDIUM = "medium", "Medium"
+    LARGE = "large", "Large"
+    ENORMOUS = "enormous", "Enormous"
+    COLOSSAL = "colossal", "Colossal"
+    ICE = "ice", "Ice"
